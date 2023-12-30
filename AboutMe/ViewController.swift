7468843523897498d52f15c65e8eb49577baa819
strkglya <alealehandra@mail.ru>
2023-12-30 18:04:49 +0300

    proj created

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Александра Среднева on 30.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
