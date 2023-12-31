1c9cfece05a3cb77816a5bdfd07763c18074a7f7
strkglya <alealehandra@mail.ru>
2023-12-31 19:24:53 +0300

    first screen done

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -10,20 +10,46 @@ import SnapKit
 
 class ViewController: UIViewController {
 
+    let secondVC = SecondViewController()
+    
     override func viewDidLoad() {
+
         super.viewDidLoad()
         
-        let box = UIView()
-        box.backgroundColor = .blue
-        view.addSubview(box)
-        box.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = #colorLiteral(red: 0.3027401567, green: 0.6057469845, blue: 1, alpha: 1)
         
-        box.snp.makeConstraints { make in
-            make.top.bottom.left.right.equalTo(self.view)
+        let greetings = UILabel()
+        greetings.text = "Привет! \nМеня зовут Саша, и это моя визитка"
+        greetings.numberOfLines = 0
+        greetings.textColor = .white
+        greetings.textAlignment = .center
+        greetings.font = UIFont(name: "AvenirNext-Medium", size: 32)
+        greetings.sizeToFit()
+        view.addSubview(greetings)
+        
+        let nextScreenButton = UIButton()
+        nextScreenButton.setTitle("Дальше", for: .normal)
+        nextScreenButton.addTarget(self, action: #selector(nextScreen), for: .touchUpInside)
+        view.addSubview(nextScreenButton)
+
+        greetings.translatesAutoresizingMaskIntoConstraints = false
+        greetings.snp.makeConstraints { make in
+            make.left.equalTo(40)
+            make.right.equalTo(view).offset(-40)
+            make.centerY.equalTo(view)
         }
-        // Do any additional setup after loading the view.
+        
+        nextScreenButton.translatesAutoresizingMaskIntoConstraints = false
+        nextScreenButton.snp.makeConstraints { make in
+            make.bottom.equalTo(-40)
+            make.width.equalTo(100)
+            make.centerX.equalTo(view)
+        }
     }
-
-
+    
+    @objc private func nextScreen(){
+        guard let navVC = navigationController else {return}
+        navVC.pushViewController(secondVC, animated: true)
+    }
 }
 
