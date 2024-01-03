fff86c3ba4738a0c38b41b212c6d40a36d6d8028
strkglya <alealehandra@mail.ru>
2024-01-03 20:19:31 +0300

    last screen done

--- AboutMe/LastViewController.swift
+++ AboutMe/LastViewController.swift
@@ -1,29 +1,59 @@
-//
-//  LastViewController.swift
-//  AboutMe
-//
-//  Created by Александра Среднева on 3.01.24.
-//
-
 import UIKit
+import SnapKit
 
 class LastViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .purple
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = #colorLiteral(red: 0.3027401567, green: 0.6057469845, blue: 1, alpha: 1)
+
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.spacing = 10
+
+        let catStack = UIStackView()
+        catStack.axis = .vertical
+        catStack.alignment = .center
+        catStack.distribution = .fillEqually
+        catStack.spacing = 2
+
+        let aboutCat = UILabel()
+        aboutCat.text = "А еще у меня есть кот, его зовут Рамзи. Он любит заполнять товарно-транспортные накладные, есть бумажки и грызться"
+        aboutCat.numberOfLines = 0
+        aboutCat.textColor = .white
+        aboutCat.textAlignment = .center
+        aboutCat.font = UIFont(name: "AvenirNext-Medium", size: 18)
+        aboutCat.sizeToFit()
+
+        let catOnChair = UIImageView(image: UIImage(named: "catOnChair"))
+        catOnChair.contentMode = .scaleToFill
+
+        let catWorking = UIImageView(image: UIImage(named: "catWorking"))
+        catWorking.contentMode = .scaleToFill
+
+        let catSmoking = UIImageView(image: UIImage(named: "catSmoking"))
+        catSmoking.contentMode = .scaleToFill
+
+        catStack.addArrangedSubview(catOnChair)
+        catStack.addArrangedSubview(catWorking)
+        catStack.addArrangedSubview(catSmoking)
+
+        stackView.addArrangedSubview(catStack)
+        stackView.addArrangedSubview(aboutCat)
+
+        view.addSubview(stackView)
+
+        stackView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(10)
+            make.bottom.equalTo(-60)
+            make.left.equalTo(20)
+            make.right.equalTo(-20)
+        }
+
+        catStack.snp.makeConstraints { make in
+            make.width.equalTo(stackView.snp.width).multipliedBy(0.6)
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
