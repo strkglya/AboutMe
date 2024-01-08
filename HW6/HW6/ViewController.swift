c2eb9c0a79b6cf7ba956baa9d437cda8795314eb
strkglya <alealehandra@mail.ru>
2024-01-08 22:49:27 +0300

    part of first screen

--- HW6/HW6/ViewController.swift
+++ HW6/HW6/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userImage: UIImageView!
+    
+    @IBOutlet weak var placeholder: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        placeholder.text = "No Image"
+        userImage.layer.cornerRadius = userImage.frame.size.width/2
+        userImage.layer.masksToBounds = true
+        userImage.layer.borderWidth = 2
+        userImage.layer.borderColor = UIColor.purple.cgColor
+        userImage.backgroundColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+        
+        userImage.tintColor = .placeholderText
+        
     }
 
 
