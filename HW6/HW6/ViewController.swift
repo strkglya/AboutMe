d2a7b00d73b24b1258d833b84253e54107464b67
strkglya <alealehandra@mail.ru>
2024-01-09 12:34:16 +0300

    first screen without horizontal

--- HW6/HW6/ViewController.swift
+++ HW6/HW6/ViewController.swift
@@ -1,31 +1,58 @@
-//
-//  ViewController.swift
-//  HW6
-//
-//  Created by Александра Среднева on 8.01.24.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var userImage: UIImageView!
-    
     @IBOutlet weak var placeholder: UILabel!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var customizeButton: UIButton!
+    @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+
+    private var isNewScreen = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        placeholder.text = "No Image"
+        setUpScreen()
+        isNew()
+    }
+    
+    private func setUpScreen() {
+        
         userImage.layer.cornerRadius = userImage.frame.size.width/2
         userImage.layer.masksToBounds = true
         userImage.layer.borderWidth = 2
         userImage.layer.borderColor = UIColor.purple.cgColor
         userImage.backgroundColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
         
-        userImage.tintColor = .placeholderText
+        placeholder.text = "No Image"
         
+        customizeButton.layer.borderWidth = 1
+        customizeButton.layer.borderColor = UIColor.purple.cgColor
+        customizeButton.layer.cornerRadius = 10
+        customizeButton.setTitleColor(.purple, for: .normal)
     }
-
-
+    
+    private func isNew(){
+        if isNewScreen {
+            saveButton.isHidden = false
+            editButton.isHidden = false
+            cancelButton.isHidden = false
+            customizeButton.isHidden = true
+        } else {
+            saveButton.isHidden = true
+            editButton.isHidden = true
+            cancelButton.isHidden = true
+        }
+    }
+    
+    @IBAction func customizeAction(_ sender: Any) {
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let secondVC = mainStoryboard.instantiateViewController(withIdentifier: "ViewController") as? ViewController else {return}
+        secondVC.isNewScreen = true
+        present(secondVC, animated: true)
+    }
 }
 
